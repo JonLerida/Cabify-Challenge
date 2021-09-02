3ee60098f1feb3af1d451a04a080160fc86d6bd4
JonLerida <jlerida@pa.uc3m.es>
2021-09-02 18:42:54 +0200

    Server skeleton almost ended Request parser (possibly) ended response_template.html modified (needs more work)

--- request.py
+++ request.py
@@ -1,21 +1,69 @@
-from string import Template
-
+from io import StringIO
+import email
 
 class Request(object):
     def __init__(self, raw_request):
         self.raw_request = raw_request
+        self.headers = None
+        self.method = None
+        self.full_path = None
+        self.resource = None
+        self.protocol = None
+        self.headers = None
+        self.first_line = None
+        self.arguments = None
+
         self.parse_request(self.raw_request)
 
-        self.headers = None
-        # todo fix this
-        self.method = 'GET'
+    def _parse_path(self, path):
+        if '?' in path:
+            self.resource, arguments = path.split('?', 1)
+            arguments = arguments.replace('=', ':')
+            aux = email.message_from_file(StringIO(arguments))
+            self.arguments = dict(aux.items())
+        else:
+            self.resource = path.strip()
+            self.arguments = {}
 
-    def parse_request(self, raw_request):
-        raw_request.split('\r\n')
+
+    def _parse_first_line(self, first_line):
+        parts = first_line.split(' ')
+        self.path = parts[1]
+        self._parse_path(self.path)
+        self.method = parts[0]
+        self.protocol = parts[2]
+
+    def _parse_headers(self, headers):
         pass
 
+    def parse_request(self, raw_request):
+        self.first_line, headers = raw_request.split('\r\n', 1)
+
+        self._parse_first_line(self.first_line)
+        # construct a message from the request string
+        message = email.message_from_file(StringIO(headers))
+
+        # construct a dictionary containing the headers
+        self.headers = dict(message.items())
+
     def __str__(self):
-        return ''
+        return 'request object'
 
     def __repr__(self):
         return str(self)
+
+    def get_header_value(self, header_name):
+        """
+        returns the value of the header_name. Returns none if no header with that name
+        :param header_name: str
+        :return: str
+        """
+        return self.headers.get(header_name, '')
+
+    def get_argument_value(self, argument_name):
+        """
+        returns the value of the argument asked
+        :param argument_name: str
+        :return: str
+        """
+        return self.arguments.get(argument_name, '')
--- server.py
+++ server.py
@@ -16,17 +16,13 @@ with open('response_template.html', 'r') as f:
 
 
 # TODO move this to a new method | class
-d = {
-    'code': 200,
-    'response': 'Ok',
-    'connection_header': 'Closed',
-    'content_type_header': 'text/html'
-}
+
 
 
 class Server(object):
     def __init__(self):
-        pass
+        # TODO create here variables needed (such as lists)
+        self.journey_counter = 0
 
     def connection_handler(self):
         """
@@ -49,10 +45,11 @@ class Server(object):
         s.listen()
         while True:
             conn, addr = s.accept()
-            print("[CONNECT] New connection!")
-            self.authentication(conn, addr)
+            print("[CONNECT] New connection")
+            self.request_handler(conn, addr)
+            conn.close()
 
-    def authentication(self, conn, addr):
+    def request_handler(self, conn, addr):
         """
 
         :param conn:
@@ -61,30 +58,191 @@ class Server(object):
         :raises: generic exception
         """
         try:
-            request = Request(conn.recv(2048).decode())
-            print(request)
+            # Parses the HTTP request
+            data = conn.recv(1024).decode('utf8', 'strict')
+            if not data:
+                print("Empty data received")
+                return
+
+            request = Request(data)
 
             if request.method == 'GET':
-                # todo do get
-                generic_http_response_result = generic_http_response.substitute(d)
-                generic_html_response_result = generic_html_response.substitute(d)
-                generic_http_response_result += generic_html_response_result
-                conn.sendall(generic_http_response_result.encode())
+                self.do_GET(conn, request)
             elif request.method == 'PUT':
                 # todo put
-                pass
+                self.do_PUT(conn, request)
             elif request.method == 'POST':
                 # todo post
-                pass
+                self.do_POST(conn, request)
             else:
                 # todo either raise and exception or send an error response
-                pass
+                self.do_DEFAULT(conn, request)
         except Exception as e:
             print("[EXCEPTION]", e)
+            # todo do we wanna actually close the socket?
             conn.close()
+
+    def do_GET_status(self, conn, request):
+        """
+
+        :param conn:
+        :param request:
+        :return:
+        """
+
+        d = {
+            'code': 200,
+            'response': 'Ok',
+            'connection_header': 'Closed',
+            'content_type_header': 'text/html',
+            'server_message': "SERVER'S UP"
+        }
+
+        generic_http_response_result = generic_http_response.substitute(d)
+        generic_html_response_result = generic_html_response.substitute(d)
+        generic_http_response_result += generic_html_response_result
+        conn.sendall(generic_http_response_result.encode())
+        print("[LOG] GET status resource")
+
+    def do_GET_invalid(self, conn, request):
+        """
+
+        :param conn:
+        :param request:
+        :return:
+        """
+
+        d = {
+            'code': 404,
+            'response': 'Not Found',
+            'connection_header': 'Closed',
+            'content_type_header': 'text/html',
+            # 'server_message': 'SERVER UP'
+        }
+
+        generic_http_response_result = generic_http_response.safe_substitute(d)
+        generic_html_response_result = generic_html_response.safe_substitute(d)
+        generic_http_response_result += generic_html_response_result
+        conn.sendall(generic_http_response_result.encode())
+        print("[LOG] GET invalid resource")
+
+    def do_GET(self, conn, request):
+        """
+        bnla bla
+
+        :param conn:
+        :param request:
+        :return:
+        """
+        # todo do get
+        if request.resource == '/status':
+            self.do_GET_status(conn, request)
+        else:
+            self.do_GET_invalid(conn, request)
+
+    def do_POST(self, conn, request):
+        """
+        hace...
+
+        :param conn: socket
+        :param request: Request
+        :return: None
+        """
+        if request.resource == '/journey':
+            self.do_POST_journey(conn, request)
+        elif request.resource == '/dropoff':
+            self.do_POST_dropoff(conn, request)
+        elif request.resource == '/locate':
+            self.do_POST_locate(conn, request)
+        else:
+            self.do_POST_invalid(conn, request)
+
+    def do_POST_journey(self, conn, request):
+        """
+        hace
+
+        :param conn:
+        :param request:
+        :return:
+        """
+        pass
+
+    def do_POST_dropoff(self, conn, request):
+        """
+        hace
+
+        :param conn:
+        :param request:
+        :return:
+        """
+        pass
+
+    def do_POST_locate(self, conn, request):
+        """
+        hace
+
+        :param conn:
+        :param request:
+        :return:
+        """
+        pass
+
+    def do_POST_invalid(self, conn, request):
+        """
+        ghace
+
+        :param conn:
+        :param request:
+        :return:
+        """
+        pass
+
+    def do_PUT(self, conn, request):
+        """
+        hace
+
+        :param conn:
+        :param request:
+        :return:
+        """
+        if request.resource == '/cars':
+            self.do_PUT_cars(conn, request)
+        else:
+            self.do_PUT_invalid(conn, request)
+
+    def do_PUT_invalid(self, conn, request):
+        """
+        hace
+
+        :param conn:
+        :param request:
+        :return:
+        """
+        pass
+
+    def do_PUT_cars(self, conn, request):
+        """
+        hace
+
+        :param conn:
+        :param request:
+        :return:
+        """
+        pass
+
+    def do_DEFAULT(self, conn, request):
+        """
+        hace
+
+        :param conn:
+        :param request:
+        :return:
+        """
+        pass
 
 
 if __name__ == '__main__':
+
     server = Server()
     # Launch a thread binded to a socket and listen all requests
     t = threading.Thread(target=server.connection_handler)
